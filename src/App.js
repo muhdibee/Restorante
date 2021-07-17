6fe5b202ce5cfe121ea90383fb5bfdb906ff83f7
Muhammad <muhdibee@gmail.com>
2021-07-17 17:43:39 +0100

    Components Part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import MenuComponent from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand>Restorante</NavbarBrand>
         </div>
       </Navbar>
+      <MenuComponent/>
     </div>
   );
 }
